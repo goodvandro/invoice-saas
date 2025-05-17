8af2267b7baf927fa31f532a1bcf86ef4db9611c
Evandro Monteiro Lima <goodvandro@gmail.com>
2025-05-17 19:23:16 +0000

    feat: add tenant context middleware and update configurations
    Introduce TenantContextMiddleware to handle tenant-specific requests. Update Prettier, ESLint, and TypeScript configurations for better code formatting and type checking. Add Docker Compose setup for MongoDB and modify app module to include Mongoose for database connectivity.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TenantContextMiddleware } from './common/middlewares/tenant-context.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(new TenantContextMiddleware().use);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
